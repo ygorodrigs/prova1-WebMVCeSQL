0388cbb82ccd8c709d77c89358ddfd7691569927
ygorodrigs <ygorodrigs@gmail.com>
2026-09-10 21:21:58 -0300

    parte-d: defeitos corrigidos

--- src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
+++ src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
@@ -1,19 +1,21 @@
 package br.edu.iftm.petvida.controller;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.edu.iftm.petvida.repository.AnimalRepository;
- 
-public class ConsultaController {                          // <== ITEM 1
- 
+
+@Controller // ITEM 1 Corrigido
+public class ConsultaController {
+
     @Autowired
-    AnimalRepository repository;
- 
-    @GetMapping("consulta")                                // <== ITEM 2
+    private AnimalRepository repository;
+
+    @GetMapping("consulta") // ITEM 2 (Já estava correto)
     public String consulta(Model model) {
-        model.addAttribute("bicho", repository.buscarPorId(1));   // <== ITEM 3
-        return "consulta.html";                            // <== ITEM 4
+        model.addAttribute("animal", repository.buscarPorId(1)); // ITEM 3 Corrigido ("animal" em vez de "bicho")
+        return "consulta"; // ITEM 4 Corrigido ("consulta" em vez de "consulta.html")
     }
 }
